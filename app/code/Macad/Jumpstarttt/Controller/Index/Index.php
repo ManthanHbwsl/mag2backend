0d14efaea3bd86e752a92591a2dbb796c44bc1fe
manthan <manthan.shinde@hbwsl.com>
2025-01-29 19:07:41 +0530

    added routes.xml

---
+++ app/code/Macad/Jumpstarttt/Controller/Index/Index.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Macad\Jumpstarttt\Controller\Index;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+class Index implements HttpGetActionInterface
+{
+    public function execute()
+    {
+        die('Jumpstart!');
+    }
+}
--- app/code/Macad/Jumpstarttt/registration.php
+++ app/code/Macad/Jumpstarttt/registration.php
@@ -3,6 +3,6 @@ declare(strict_types=1);
 use Magento\Framework\Component\ComponentRegistrar;
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Macadcd_Jumpstart',
+    'Macad_Jumpstarttt',
     __DIR__,
 );

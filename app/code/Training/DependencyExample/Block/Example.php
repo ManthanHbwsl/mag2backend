2b6c4b1faba5a77b6b1dfac7626891c00d727784
manthan <manthan.shinde@hbwsl.com>
2025-02-19 14:46:10 +0530

    refactor Example block class for improved code style

--- app/code/Training/DependencyExample/Block/Example.php
+++ app/code/Training/DependencyExample/Block/Example.php
@@ -1,13 +1,14 @@
 <?php
 
-declare(strict_types= 1);
+declare(strict_types=1);
 
 namespace Training\DependencyExample\Block;
 
+use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 
-class Example extends Template{
-    public function __construct(Context $context, array $data = []){
+class Example extends Template {
+    public function __construct(Context $context, array $data = []) {
         parent::__construct($context, $data);
     }
 }
